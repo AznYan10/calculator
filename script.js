2f5bf1990d9ac32e938e48a02681dc948e115b91
aznyan <aznyan5510@gmail.com>
2023-04-15 20:27:35 -0400

    Created functions for addition, subtraction, multiplication, and division

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = function (x,y) {
+    return x + y;
+};
+
+const subtract = function (x,y) {
+    return x - y;
+};
+
+const multiply = function (x,y) {
+    return x * y;
+};
+
+const divide = function (x,y) {
+    return x / y;
+};
