7b34d270c5376123f8f1d2bc52521142b636c2d6
aznyan <aznyan5510@gmail.com>
2023-04-18 00:44:27 -0400

    Created operate function asking user what operator to use and numbers. Will call one of the operator functions depedning on the user input

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-const add = function (x,y) {
-    return x + y;
+const add = function (x, y, operator) {
+    operator = x + y;
+    return operator;
 };
 
-const subtract = function (x,y) {
-    return x - y;
+const subtract = function (x, y, operator) {
+    operator =  x - y;
+    return operator;
 };
 
-const multiply = function (x,y) {
-    return x * y;
+const multiply = function (x, y, operator) {
+    operator = x * y;
+    return operator;
 };
 
-const divide = function (x,y) {
-    return x / y;
+const divide = function (x, y, operator) {
+    operator = x / y;
+    return operator;
 };
+
+const operate = function (x, y, operator) {
+    operator = prompt('Add, subtract, multiply, or divide');
+    x = parseInt(prompt('Enter 1st number: '));
+    y = parseInt(prompt('Ener 2nd number: '));
+
+    if (operator === 'add') {
+         console.log(add(x,y));
+    } else if (operator === 'subtact') {
+        console.log(subtract(x,y));
+    } else if(operator === 'multiply') {
+        console.log(multiply(x,y));
+    } else if(operator === 'divide') {
+        console.log(divide(x,y));
+    } else {
+        console.log('Incorrect input');
+    }
+}
+
+operate();
