88adb84cf3a2c15db1f5d2147a520a64237a2ffd
aznyan <aznyan5510@gmail.com>
2023-04-20 01:58:13 -0400

    created numbers, opeartor, and equaulbtn elements when the buttons are clicked and created a function for each one

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// All necessary elements
+const numbers = document.querySelectorAll('.button');
+const operators = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('#equal');
+
+// operator functions
 const add = function (x, y, operator) {
     operator = x + y;
     return operator;
@@ -18,7 +24,8 @@ const divide = function (x, y, operator) {
     return operator;
 };
 
-const operate = function (x, y, operator) {
+// function to choose which operator to use
+const operate = function (x, operator, y) {
     operator = prompt('Add, subtract, multiply, or divide');
     x = parseInt(prompt('Enter 1st number: '));
     y = parseInt(prompt('Ener 2nd number: '));
@@ -33,7 +40,30 @@ const operate = function (x, y, operator) {
         console.log(divide(x,y));
     } else {
         console.log('Incorrect input');
-    }
+    }equalBtn
 }
 
-operate();
+// storing numbers when clicked
+let num = 0;
+const storeNumber = function (number) {
+    num = parseInt(number.target.id);
+    console.log(num);
+}
+
+// storing operator when clicked
+let ope = '';
+const storeOperator = function (operator) {
+    ope = operator.target.id;
+    console.log(ope);
+}
+
+// operates when equal button clicked
+equalBtn.addEventListener('click', function () {
+    operate(num, ope, num);
+});
+
+// operate();
+
+// calling function when buttons are clicked
+numbers.forEach(num => num.addEventListener('click', storeNumber));
+operators.forEach(ope => ope.addEventListener('click', storeOperator));
