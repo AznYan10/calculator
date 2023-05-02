7e1afe0db68366a6d078538636b08dadef547c61
aznyan <aznyan5510@gmail.com>
2023-05-02 17:59:48 -0400

    created event listener for the operators, numbers, equal, clear, and delete button

--- script.js
+++ script.js
@@ -1,69 +1,60 @@
 // All necessary elements
-const numbers = document.querySelectorAll('.button');
+const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 const equalBtn = document.querySelector('#equal');
+const clrBtn = document.querySelector('.clear');
+const delBtn = document.querySelector('.delete');
+const plusMinusBtn = document.querySelector('plusminus');
 
-// operator functions
-const add = function (x, y, operator) {
+// calculator object
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: false,
+ };
+
+// functions for operators 
+function add (x, y, operator) {
     operator = x + y;
     return operator;
-};
+}
 
-const subtract = function (x, y, operator) {
-    operator =  x - y;
+function subtract (x, y, operator) {
+    operator = x - y;
     return operator;
-};
+}
 
-const multiply = function (x, y, operator) {
+function multiply (x, y, operator) {
     operator = x * y;
     return operator;
-};
+}
 
-const divide = function (x, y, operator) {
+function divide (x, y, operator) {
     operator = x / y;
     return operator;
-};
-
-// function to choose which operator to use
-const operate = function (x, operator, y) {
-    operator = prompt('Add, subtract, multiply, or divide');
-    x = parseInt(prompt('Enter 1st number: '));
-    y = parseInt(prompt('Ener 2nd number: '));
-
-    if (operator === 'add') {
-         console.log(add(x,y));
-    } else if (operator === 'subtact') {
-        console.log(subtract(x,y));
-    } else if(operator === 'multiply') {
-        console.log(multiply(x,y));
-    } else if(operator === 'divide') {
-        console.log(divide(x,y));
-    } else {
-        console.log('Incorrect input');
-    }equalBtn
 }
 
-// storing numbers when clicked
-let num = 0;
-const storeNumber = function (number) {
-    num = parseInt(number.target.id);
-    console.log(num);
-}
-
-// storing operator when clicked
-let ope = '';
-const storeOperator = function (operator) {
-    ope = operator.target.id;
-    console.log(ope);
-}
-
-// operates when equal button clicked
-equalBtn.addEventListener('click', function () {
-    operate(num, ope, num);
+numbers.forEach(button => {
+    button.addEventListener('click', () => {
+    
+    })
 });
 
-// operate();
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
 
-// calling function when buttons are clicked
-numbers.forEach(num => num.addEventListener('click', storeNumber));
-operators.forEach(ope => ope.addEventListener('click', storeOperator));
+    })
+});
+
+equalBtn.addEventListener('click', button => {
+
+});
+
+clrBtn.addEventListener('click', button => {
+
+});
+
+delBtn.addEventListener('click', button => {
+
+});
