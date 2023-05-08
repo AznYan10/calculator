fc08e73fae4d6723128f4d237f25a13c8ad59ca1
aznyan <aznyan5510@gmail.com>
2023-05-08 18:43:05 -0400

    Updated event listner to used swtich case instead of if statement

--- script.js
+++ script.js
@@ -90,31 +90,33 @@ updateDisplay();
 
 const keys = document.querySelector('.keys');
 keys.addEventListener('click', (event) => {
-    // access clicked element 
     const {target} = event;
+    const { value } = target;
 
     if (!target.matches('button')) {
         return;
     }
 
-    if (target.classList.contains('operator')) {
-        handleOperator(target.value);
-        updateDisplay();
-        return;
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal(value);
+            break;
+        case 'clear':
+            resetCalculator();
+            break;
+        default:
+            // check if key is an integer
+            if (Number.isInteger(parseFloat(value))) {
+                inputDigit(value);
+            }
     }
 
-    if (target.classList.contains('decimal')) {
-        inputDecimal(target.value);
-        updateDisplay();
-        return;
-    }
-
-    if (target.classList.contains('clear')) {
-        resetCalculator();
-        updateDisplay();
-        return;
-    }
-
-    inputDigit(target.value);
     updateDisplay();
 });
